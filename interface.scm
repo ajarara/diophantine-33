ca240cd074b8ec9a846c5d6bd9f97c6c85b28d94
Ahmad Jarara <floor13@jarmac.org>
2016-04-22 16:06:41 -0400

    interface redesign

--- interface.scm
+++ interface.scm
@@ -1,14 +1,22 @@
 (define helpstring
   "Incorrect command. Currently supported commands are:
-  \tq : display where we're at, quit the program
   \tc : display where we're at
+  \tv : toggle verbosity, at the cost of slowing down
+  \tw : write the results of the next try to file, and quit
   \tanything else: display this message. It's free!")
 
 (define input-port (current-input-port))
 
 (define (handle-input signal current-list)
-  (cond ((eq? signal `v)
-	 ; verbose mode, spew out all tries
-	 (verbose-loop current-list))
-	((eq? signal `c)
-	 (one-off-calc current-list)
+  (cond ((eq? signal `c)
+	 ; print the next try, and then resume silent calculation
+	 (display-one-off-and-resume current-list))
+	((eq? signal `v)
+	 ; need a way to neatly toggle verbosity
+	 (toggle-verbosity current-list))
+	((eq? signal `w)
+	 (write-out current-list))
+	(else
+	  (display helpstring)
+	  (resume current-list))
+	 
